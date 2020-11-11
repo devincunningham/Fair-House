4c5604aace71c5e9241efdd3f5015a272457fc34
Devin Cunningham <dcunningham725@gmail.com>
2020-11-11 00:09:30 -0800

    Initial commit

---
+++ MisterGorbotchev.py
@@ -0,0 +1,30 @@
+import random
+
+from discord.ext import commands
+from datetime import datetime, timedelta
+
+gorbotchev = commands.Bot(command_prefix='!')
+
+
+@gorbotchev.event
+async def on_ready():
+    print("Hello comrade.")
+
+
+@gorbotchev.command(aliases=['Solidarity'])
+@commands.has_permissions(manage_messages=True)
+async def solidarity(ctx):
+    await ctx.send("Forever!")
+ 
+
+@gorbotchev.event
+async def on_message(message):
+    if message.author == gorbotchev.user:
+        return
+
+    triggers = ['geetle', 'georgio']
+    if any(trigger in message.content.lower() for trigger in triggers):
+        if random.choice([True, False]):
+            await message.channel.send('*Get that Geetle!*')
+
+gorbotchev.run('Nzc1MTcyMTI3MTg2ODc4NDc0.X6idTQ.toLVXhf3pMvho5XsNaN1JLRImgY')
