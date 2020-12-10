4b62f2a60b9709b9fdb73b4eda85fc80aa4b0ede
Devin Cunningham <dcunningham725@gmail.com>
2020-12-09 22:12:05 -0800

    some monke banans

--- monke.py
+++ monke.py
@@ -1,29 +1,25 @@
 import random
 
 from discord.ext import commands
+import discord
 
-monke = commands.Bot(command_prefix='!')
+
+monke = discord.Client()
 
 
 @monke.event
 async def on_ready():
-    print("Hello comrade.")
-
-
-@monke.command(aliases=['Solidarity'])
-@commands.has_permissions(manage_messages=True)
-async def solidarity(ctx):
-    await ctx.send("Forever!")
+    test_channel = monke.get_channel(775175775644811264)
+    await test_channel.send("Hello comrade.")
 
 
 @monke.event
-async def on_message(message):
+async def on_banana(message):
     if message.author == monke.user:
         return
 
-    triggers = ['geetle', 'georgio']
-    if any(trigger in message.content.lower() for trigger in triggers):
-        if random.choice([True, False]):
-            await message.channel.send('*Get that Geetle!*')
+    if 'banan' in message.content.lower():
+        await message.channel.send(random.choice(['banana!', 'banan?!', 'mmmm,, banana!']))
+
 
 monke.run('Nzc1MTcyMTI3MTg2ODc4NDc0.X6idTQ.toLVXhf3pMvho5XsNaN1JLRImgY')
